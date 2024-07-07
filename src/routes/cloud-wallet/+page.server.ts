b9ab18dded1b237fbf40be5a191f3fc86adbfd5b
Sowham Bhuin <sb2517@it.jgec.ac.in>
2024-07-07 16:42:34 +0530

    Added update , Raw wallet , Cloud Wallet and Bounty page

--- src/routes/cloud-wallet/+page.server.ts
+++ src/routes/cloud-wallet/+page.server.ts
@@ -1,8 +1,69 @@
+import  NeucronSDK from 'neucron-sdk';
+
+
+
+
 /** @type {import('./$types').Actions} */
 export const actions = {
-    create: async ({ request }) => {
+    login: async ({ request }) => {
         const data = await request.formData();
+        const neucron = new NeucronSDK();
+        const authModule = neucron.authentication;
+        const walletModule = neucron.wallet;
 
-        return { success: true };
+        const loginResponse = await authModule.login({
+           email : data.get ('email'),
+           password : data.get('password'),
+        });
+        console.log(loginResponse);
+        const DefaultWalletBalance = await walletModule.getWalletBalance({});
+        console.log(DefaultWalletBalance);
+
+        // const addresses = await walletModule.getAddressesByWalletId({});
+        // console.log(addresses);
+
+
+        // const walletHistory = await walletModule.getWalletHistory({});
+        // console.log(walletHistory);
+
+        return { success: true , balance: DefaultWalletBalance.data.balance.summary};
+    },
+    pay: async ({ request }) => {
+        const data = await request.formData();
+        const neucron = new NeucronSDK();
+        const authModule = neucron.authentication;
+        const walletModule = neucron.wallet;
+
+        const loginResponse = await authModule.login({
+           email : data.get ('email'),
+           password : data.get('password'),
+        });
+        console.log(loginResponse);
+        const DefaultWalletBalance = await walletModule.getWalletBalance({});
+        console.log(DefaultWalletBalance);
+
+        // const addresses = await walletModule.getAddressesByWalletId({});
+        // console.log(addresses);
+
+        const options ={
+            outputs: [
+                {
+                    address: data.get('paymail'),
+                    note : 'guru pay',
+                    amount: data.get('amount'),
+                },
+            ],  
+        }
+
+        const payResponse = await neucron.pay.txSpend(options);
+        console.log(payResponse);
+
+        // const walletHistory = await walletModule.getWalletHistory({});
+        // console.log(walletHistory);
+
+        return { success: true , payResponse: payResponse};
     }
 };
+
+
+
--- src/routes/cloud-wallet/+page.svelte
+++ src/routes/cloud-wallet/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+     /** @type {import('./$types').ActionData} */
+  export let form;
+</script>
+
 <div
     class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
 >
@@ -44,6 +49,85 @@
     </form>
 
     {#if form?.success}
-        <p class="pt-2">Log In Successful</p>
+        <p class="pt-2">Log In Successful , Your balance is {form?.balance}</p>
+    {/if}
+</div>
+<div
+    class="mt-10 pt-10 w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
+>
+    <form method="POST" action="?/pay">
+        <div class="flex flex-wrap -mx-3 mb-2">
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="email"
+                >
+                    Email
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="email"
+                    type="text"
+                    placeholder="Enter email"
+                    name="email"
+                />
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="grid-name"
+                >
+                    Password
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="password"
+                    type="text"
+                    placeholder="Enter password"
+                    name="password"
+                />
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="paymail"
+                >
+                    Paymail
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="paymail"
+                    type="text"
+                    placeholder="Enter paymail"
+                    name="paymail"
+                />
+            </div>
+            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                <label
+                    class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                    for="amount"
+                >
+                    Amount
+                </label>
+                <input
+                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                    id="amount"
+                    type="text"
+                    placeholder="Enter amount"
+                    name="amount"
+                />
+            </div>
+            
+            <button
+                type="submit"
+                class="bg-yellow-500 hover:bg-blue-700 text-white font-bold mt-5 ml-2 px-2 rounded"
+            >
+                Pay
+            </button>
+        </div>
+    </form>
+
+    {#if form?.success}
+        <p class="pt-2">Payment Successful,Here is your <a href={form?.balance}>transaction</a>  </p>
     {/if}
 </div>
